859d3be617722b34b285b8cc81cefd1ecbd9e2be
evve0119 <58882040+evve0119@users.noreply.github.com>
2026-04-05 22:06:22 -0400

    1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '//learnd/',
+  base: '/slide_present/',
   plugins: [
     react(),
     tailwindcss(),
